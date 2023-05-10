e1981f34634d1fdecd837f4a43df86aa131b7700
IsraelSanabriaMx <israel.sanabriamx@gmail.com>
2023-05-10 12:42:16 -0600

    Init repo

--- src/index.js
+++ src/index.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+
+// 1
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache
+} from '@apollo/client';
+
+import './styles/index.css';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+
+// 2
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+});
+
+// 3
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
